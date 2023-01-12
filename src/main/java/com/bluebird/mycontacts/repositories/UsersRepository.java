e651c993309202fe3a7591b4498df0a47b9f8ee2
BaxtiyorjonAbduqahhorov <baxtiyorjonabduqahhorov2004@gmail.com>
2023-01-12 16:34:03 +0500

    3rd commit

---
+++ src/main/java/com/bluebird/mycontacts/repositories/UsersRepository.java
@@ -0,0 +1,13 @@
+package com.bluebird.mycontacts.repositories;
+
+import com.bluebird.mycontacts.entities.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
