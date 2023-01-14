decceaae3923fc73496d97ce79ba8eca6902e938
BaxtiyorjonAbduqahhorov <baxtiyorjonabduqahhorov2004@gmail.com>
2023-01-14 14:48:54 +0500

    3rd commit

---
+++ src/main/java/com/bluebird/mycontacts/extra/RegisterResult.java
@@ -0,0 +1,13 @@
+package com.bluebird.mycontacts.extra;
+
+public class RegisterResult {
+
+    private boolean status;
+
+    private String message;
+
+    public RegisterResult(boolean status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
--- src/main/java/com/bluebird/mycontacts/security/SecurityConfiguration.java
+++ src/main/java/com/bluebird/mycontacts/security/SecurityConfiguration.java
@@ -2,12 +2,23 @@ package com.bluebird.mycontacts.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
+
+    public final TokenFilter tokenFilter;
+
+    public SecurityConfiguration(TokenFilter tokenFilter) {
+        this.tokenFilter = tokenFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,6 +31,17 @@ public class SecurityConfiguration {
                 .httpBasic()
                 .authenticationEntryPoint(new AuthEntryPoint());
 
+        http.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
