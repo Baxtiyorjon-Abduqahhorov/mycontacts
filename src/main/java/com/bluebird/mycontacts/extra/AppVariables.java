9b8bfcbe926c9a6c9ea092a942a9be0df2844206
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2023-02-05 14:23:46 +0500

    app path solved

--- src/main/java/com/bluebird/mycontacts/extra/AppVariables.java
+++ src/main/java/com/bluebird/mycontacts/extra/AppVariables.java
@@ -2,7 +2,7 @@ package com.bluebird.mycontacts.extra;
 
 public class AppVariables {
 
-    public static String PATH =  System.getProperty("user.dir")+"/src/main/resources/storage/";
+    public static String PATH =  System.getProperty("user.dir").replace("/build/libs","")+"/src/main/resources/storage/";
 
     public static String IMAGE_SERVER_URL = "http://132.145.138.206:8080/api/info/picture";
 }
